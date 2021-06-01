c5bca09f18a10e74dff649cd3f095f60bc9f1a81
Dmitriy <dmitriy.babich0@gmail.com>
2021-05-31 20:30:43 -0700

    added different prompts for each position

--- index.js
+++ index.js
@@ -11,19 +11,105 @@ const questions = () => {
             choices: ['manager', 'developer', 'intern']
         }
     ])
-    .then((answer) => {
-        switch (answer.position) {
+    .then((position) => {
+        switch (position.position) {
             case 'manager':
-                console.log(' person chose manager');
+                managerCard();
                 break;
             case 'developer':
-                console.log('developer');
+                developerCard();
                 break;
             case 'intern':
-                console.log('intern');
+                internCard();
                 break;
         }
     })
 }
+
+const managerCard = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Id number: '
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'Office number: '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        },
+    ])
+    .then((answers) => {
+       console.log(answers); // calls a write that will write the answers and populate the html doc
+    })
+};
+
+const developerCard = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Id number: '
+        },
+        {
+            type: 'input',
+            name: 'git',
+            message: 'GitHub username: '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        },
+    ])
+    .then((answers) => {
+       console.log(answers); // calls a write that will write the answers and populate the html doc
+    })
+};
+
+const internCard = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Id number: '
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school are you currently enrolled in? '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        },
+    ])
+    .then((answers) => {
+       console.log(answers); // calls a write that will write the answers and populate the html doc
+    })
+}
+
+
 
 questions();
