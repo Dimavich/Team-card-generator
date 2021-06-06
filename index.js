c34cef54967d546bbff5f724e4b0e662c513d897
Dmitriy <dmitriy.babich0@gmail.com>
2021-06-06 00:18:06 -0700

    the the cards are being rendered to the html

--- index.js
+++ index.js
@@ -28,23 +28,24 @@ const employee = () => {
             message: 'What is your employee id?'
         }
     ])
-    .then(({name, role, id, email}) => {
+    .then(({name, role, email, id}) => {
+        let info ='';
         switch (role) {
             case 'manager':
-                role = 'Office Number';
+                info = 'Office Number';
                 break;
             case 'engineer':
-                role = 'Git Hub Username';
+                info = 'Git Hub Username';
                 break;
             case 'intern':
-                role = 'Current Shool';
+                info = 'Current Shool';
                 break;
         }
         inquirer.prompt([
             {
                 type: 'input',
                 name: 'uniqueInfo',
-                message: `What is your ${role}`
+                message: `What is your ${info}`
             },
             {
                 type: 'list',
@@ -54,16 +55,19 @@ const employee = () => {
             }
         ])
         .then(({uniqueInfo,done}) => {
-            const newEmployee = '';
+            let newEmployee = '';
             switch(role){
                 case 'manager':
                     newEmployee = new Manager(name, id, email, uniqueInfo);
+                    addEmployee(newEmployee);
                     break;
                 case 'engineer':
                     newEmployee = new Engineer(name, id, email, uniqueInfo);
+                    addEmployee(newEmployee);
                     break;
                 case 'intern':
                     newEmployee = new Intern(name, id, email, uniqueInfo);
+                    addEmployee(newEmployee);
                     break;
             }
             switch(done) {
@@ -77,6 +81,73 @@ const employee = () => {
 
     })
 }
+
+
+
+const addEmployee = (user) => {
+    return new Promise ((resolve, reject)=>{
+        const name = user.getName();
+        const role = user.getRole();
+        const email = user.getEmail();
+        const id = user.getId();
+
+        let card ='';
+        switch(role){
+            case 'engineer':
+                const git = user.getGit();
+                card = `<div class="card bg-danger" style="width: 18rem; margin-top: 2em;">
+                <div class="card-header text-white">
+                    ${name}
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Developer <i class="fas fa-code"></i></li>
+                    <li class="list-group-item">id: ${id}</li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${git}">${git}</a></li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                </ul>
+            </div>`
+                break;
+
+            case 'intern':
+                const school = user.getSchool();
+                card = `<div class="card bg-danger" style="width: 18rem; margin-top: 2em;">
+    <div class="card-header text-white">
+        ${name}
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Intern <i class="fas fa-graduation-cap"></i></li>
+        <li class="list-group-item">id: ${id}</li>
+        <li class="list-group-item">School: ${school}</a></li>
+        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+    </ul>
+</div>`;
+                break;
+            
+            case 'manager':
+                const office = user.getOffice();
+                card = `<div class="card bg-danger" style="width: 18rem; margin-top: 2em;">
+                <div class="card-header text-white">
+                    ${name}
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Manager <i class="fas fa-glasses"></i></li>
+                    <li class="list-group-item">id: ${id}</li>
+                    <li class="list-group-item"> Office number: ${office}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                </ul>
+            </div>`;
+                break;
+
+        }
+        fs.appendFile('./src/index.html',card,(err) => {
+            if (err) {
+                return reject(err)
+            } else {
+                return resolve;
+            }
+        });
+    })
+} 
 
 employee();
 
