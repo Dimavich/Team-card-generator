0d10fcec5eda1115ae2214f27df8ae06be58b7c8
Dmitriy <dmitriy.babich0@gmail.com>
2021-06-06 00:37:15 -0700

    added and end functionality where it adds the closing tags for the html doc after you are done generating your employee cards

--- index.js
+++ index.js
@@ -75,7 +75,8 @@ const employee = () => {
                     employee();
                     break;
                 case 'No':
-                    console.log('Input finish html function here');
+                    console.log('Generating....');
+                    endHtml();
             }
         })
 
@@ -141,163 +142,29 @@ const addEmployee = (user) => {
         }
         fs.appendFile('./src/index.html',card,(err) => {
             if (err) {
-                return reject(err)
+                console.log(err);
             } else {
-                return resolve;
+                
             }
         });
     })
 } 
+
+const endHtml = () => {
+
+    let end = `</div>
+    </div>
+    </body>
+    </html>`;
+    fs.appendFile('./src/index.html',end,(err) =>err ? console.error(err) : console.log('Success!'));
+}
 
 employee();
 
 
 
 
-// const position = () => {
-//     return inquirer.prompt([
-//         {
-//             type: 'list',
-//             name: 'position',
-//             message: 'Choose your position',
-//             choices: ['manager', 'developer', 'intern']
-//         }
-//     ])
-//     .then((position) => {
-//         switch (position.position) {
-//             case 'manager':
-//                 managerCard();
-//                 break;
-//             case 'developer':
-//                 developerCard();
-//                 break;
-//             case 'intern':
-//                 internCard();
-//                 break;
-//         }
-//     })
-// }
 
-// const managerCard = () => {
-//     return inquirer.prompt([
-//         {
-//             type: 'input',
-//             name: 'name',
-//             message: 'What is your name?'
-//         },
-//         {
-//             type: 'input',
-//             name: 'id',
-//             message: 'Id number: '
-//         },
-//         {
-//             type: 'input',
-//             name: 'office',
-//             message: 'Office number: '
-//         },
-//         {
-//             type: 'input',
-//             name: 'email',
-//             message: 'What is your email?'
-//         },
-//     ])
-//     .then((answers) => {
-//             fs.appendFile('./web/empty.html',`<div class="card bg-danger" style="width: 18rem; margin-top: 2em;">
-//             <div class="card-header text-white">
-//                 ${answers.name}
-//             </div>
-//             <ul class="list-group list-group-flush">
-//                 <li class="list-group-item">Manager <i class="fas fa-glasses"></i></li>
-//                 <li class="list-group-item">id: ${answers.id}</li>
-//                 <li class="list-group-item"> Office number: ${answers.office}</li>
-//                 <li class="list-group-item">Email: <a href="mailto:${answers.email}">${answers.email}</a></li>
-//             </ul>
-//         </div>`,
-//         (err) =>
-//         err ? console.error(err) : console.log('Success!'));
-//     })
-
-// };
-
-// const developerCard = () => {
-//     return inquirer.prompt([
-//         {
-//             type: 'input',
-//             name: 'name',
-//             message: 'What is your name?'
-//         },
-//         {
-//             type: 'input',
-//             name: 'id',
-//             message: 'Id number: '
-//         },
-//         {
-//             type: 'input',
-//             name: 'git',
-//             message: 'GitHub username: '
-//         },
-//         {
-//             type: 'input',
-//             name: 'email',
-//             message: 'What is your email?'
-//         },
-//     ])
-//     .then((answers) => {
-//         fs.appendFile('./web/empty.html',`<div class="card bg-danger" style="width: 18rem; margin-top: 2em;">
-//         <div class="card-header text-white">
-//             ${answers.name}
-//         </div>
-//         <ul class="list-group list-group-flush">
-//             <li class="list-group-item">Developer <i class="fas fa-code"></i></li>
-//             <li class="list-group-item">id: ${answers.id}</li>
-//             <li class="list-group-item">GitHub: <a href="https://github.com/${answers.git}">${answers.git}</a></li>
-//             <li class="list-group-item">Email: <a href="mailto:${answers.email}">${answers.email}</a></li>
-//         </ul>
-//     </div>`,
-//     (err) =>
-//     err ? console.error(err) : console.log('Success!'));
-//     })
-// };
-
-// const internCard = () => {
-//     return inquirer.prompt([
-//         {
-//             type: 'input',
-//             name: 'name',
-//             message: 'What is your name?'
-//         },
-//         {
-//             type: 'input',
-//             name: 'id',
-//             message: 'Id number: '
-//         },
-//         {
-//             type: 'input',
-//             name: 'school',
-//             message: 'What school are you currently enrolled in? '
-//         },
-//         {
-//             type: 'input',
-//             name: 'email',
-//             message: 'What is your email?'
-//         },
-//     ])
-//     .then((answers) => {
-//         fs.appendFile('./web/empty.html',`<div class="card bg-danger" style="width: 18rem; margin-top: 2em;">
-//         <div class="card-header text-white">
-//             ${answers.name}
-//         </div>
-//         <ul class="list-group list-group-flush">
-//             <li class="list-group-item">Intern <i class="fas fa-graduation-cap"></i></li>
-//             <li class="list-group-item">id: ${answers.id}</li>
-//             <li class="list-group-item">School: ${answers.school}</a></li>
-//             <li class="list-group-item">Email: <a href="mailto:${answers.email}">${answers.email}</a></li>
-//         </ul>
-//     </div>`,
-//     (err) =>
-//     err ? console.error(err) : console.log('Success!'));
-//     })
-// }
 
 
 
